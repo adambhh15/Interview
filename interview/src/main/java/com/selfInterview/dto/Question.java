74a97e8ccd70d3cca6f52efd8953c6d0f6b5d1b5
BangHunhyuk <bhh15@naver.com>
2023-05-23 17:07:36 +0900

    hi

---
+++ interview/src/main/java/com/selfInterview/dto/Question.java
@@ -0,0 +1,16 @@
+package com.selfInterview.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Question {
+	private int seq;
+	private String question;
+	private String category;
+}
